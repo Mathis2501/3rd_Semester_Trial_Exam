b9b6d1da236a417bbcf78f85e504dcd683e45c76
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2017-12-01 22:16:54 +0100

    started wcf practice

---
+++ WCFTrialExam/Mine/WCFPracticeExam/WCFPracticeExam/AuctionItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCFPracticeExam
+{
+    [DataContract]
+    public class AuctionItem
+    {
+        [DataMember]
+        public int ItemId { get; set; }
+        [DataMember]
+        public string ItemDescription { get; set; }
+        [DataMember]
+        public int RatingPrice { get; set; }
+        [DataMember]
+        public int BidPrice { get; set; }
+        [DataMember]
+        public string BidCustomerName { get; set; }
+        [DataMember]
+        public string BidCustomerPhone { get; set; }
+
+    }
+}
